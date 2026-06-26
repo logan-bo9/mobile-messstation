8863fa690586e4e60bb6df04d6961277d29d86da
Logan <voldemort2012@mail.de>
2026-06-27 00:57:33 +0200

    Update main.ino
    Add initial sensor + LCD firmware

--- firmware/main.ino
+++ firmware/main.ino
@@ -1,8 +1,43 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include "DHT.h"
+
+#define DHTPIN 2
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 void setup() {
   Serial.begin(9600);
+  dht.begin();
+  lcd.init();
+  lcd.backlight();
+
+  lcd.setCursor(0,0);
+  lcd.print("Messstation");
+  delay(1500);
+  lcd.clear();
 }
 
 void loop() {
-  Serial.println("Messstation läuft");
-  delay(1000);
+  float temp = dht.readTemperature();
+  float hum = dht.readHumidity();
+
+  Serial.print("Temp: ");
+  Serial.print(temp);
+  Serial.print(" C  Hum: ");
+  Serial.println(hum);
+
+  lcd.setCursor(0,0);
+  lcd.print("T:");
+  lcd.print(temp);
+  lcd.print("C");
+
+  lcd.setCursor(0,1);
+  lcd.print("H:");
+  lcd.print(hum);
+  lcd.print("%");
+
+  delay(2000);
 }
